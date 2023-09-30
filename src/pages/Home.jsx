f8f7e310c31ae0b454835dc97caa4e4066db5be6
Tinuola Adeyemi <Tinucy@yahoo.com>
2023-09-30 14:09:37 +0100

    added the Header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Container from "../components/container/Container";
+import Header from "../components/header/Header";
 
 export default function Home() {
-  return <Container />;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 }
